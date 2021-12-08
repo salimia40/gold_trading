7ee1bbdc5d9a11148f7e015574ae711d64d7d286
puya ars <alirezasalimikochi@gmail.com>
2021-12-08 13:44:01 +0330

    added push notification service

--- src/express/index.ts
+++ src/express/index.ts
@@ -5,11 +5,14 @@ export const passportMiddleware = passport.initialize();
 export const passportSessionMiddleware = passport.session();
 
 import express from "express";
+import { beamsAuth } from "../services/pushNotifications";
 
 const app = express();
 
 app.use(sessionMiddleware);
 app.use(passportMiddleware);
 app.use(passportSessionMiddleware);
+
+app.get("/pusher/beams-auth", beamsAuth);
 
 export default app;
---
+++ src/services/pushNotifications.ts
@@ -0,0 +1,42 @@
+import PushNotifications from "@pusher/push-notifications-server";
+import { Request, Response } from "express";
+
+const beamsClient = new PushNotifications({
+  instanceId: process.env.PUSHER_INSTANCE_ID!,
+  secretKey: process.env.PUSHER_KEY!,
+});
+
+export function publishToUser(user_id: number, title: string, body: string) {
+  beamsClient.publishToUsers([user_id.toString()], {
+    web: {
+      notification: {
+        title,
+        body,
+      },
+    },
+  });
+}
+
+export function publishToPublic(channel: string, title: string, body: string) {
+  beamsClient.publishToInterests([channel], {
+    web: {
+      notification: {
+        title,
+        body,
+      },
+    },
+  });
+}
+
+export function beamsAuth(req: Request, res: Response) {
+  // Do your normal auth checks here 🔒
+  //   @ts-ignore
+  const userId = req.user?.id; // get it from your auth system
+  const userIDInQueryParam = req.query["user_id"];
+  if (userId != userIDInQueryParam) {
+    res.status(401).send("Inconsistent request");
+  } else {
+    const beamsToken = beamsClient.generateToken(userId);
+    res.send(JSON.stringify(beamsToken));
+  }
+}
